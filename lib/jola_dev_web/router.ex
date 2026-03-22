4083418db26e245cf06e9b216821ad33b4d9d999
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2026-03-22 15:49:59 +0000

    Add cache headers

--- lib/jola_dev_web/router.ex
+++ lib/jola_dev_web/router.ex
@@ -1,18 +1,26 @@
 defmodule JolaDevWeb.Router do
   use JolaDevWeb, :router
 
+  @secure_headers %{
+    "strict-transport-security" => "max-age=63072000; includeSubDomains",
+    "referrer-policy" => "strict-origin-when-cross-origin",
+    "permissions-policy" => "camera=(), microphone=(), geolocation=()"
+  }
+
+  pipeline :public do
+    plug :accepts, ["html"]
+    plug :put_root_layout, html: {JolaDevWeb.Layouts, :root}
+    plug :put_secure_browser_headers, @secure_headers
+    plug :put_cdn_cache_header
+  end
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {JolaDevWeb.Layouts, :root}
     plug :protect_from_forgery
-
-    plug :put_secure_browser_headers, %{
-      "strict-transport-security" => "max-age=63072000; includeSubDomains",
-      "referrer-policy" => "strict-origin-when-cross-origin",
-      "permissions-policy" => "camera=(), microphone=(), geolocation=()"
-    }
+    plug :put_secure_browser_headers, @secure_headers
   end
 
   pipeline :api do
@@ -25,7 +33,7 @@ defmodule JolaDevWeb.Router do
   end
 
   scope "/", JolaDevWeb do
-    pipe_through :browser
+    pipe_through :public
 
     get "/", PageController, :home
     get "/about", PageController, :about
@@ -55,6 +63,10 @@ defmodule JolaDevWeb.Router do
   # end
 
   # Enable LiveDashboard in development
+  defp put_cdn_cache_header(conn, _opts) do
+    put_resp_header(conn, "cache-control", "public, s-maxage=86400, max-age=0")
+  end
+
   if Application.compile_env(:jola_dev, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.

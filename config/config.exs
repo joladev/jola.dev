346ac636b0830f6cefab71c03fe845925ed5ee9a
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2026-05-17 13:53:14 -0600

    fix

--- config/config.exs
+++ config/config.exs
@@ -44,7 +44,7 @@ config :tailwind,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: :all
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
